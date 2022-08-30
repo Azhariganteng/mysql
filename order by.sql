d74b30d63e459451b317b08f9f3f004754bc9ff7
azhariganteng <azhariramadan59@gmail.com>
2022-08-30 11:33:02 +0700

    telah selesai belajar sql

---
+++ order by.sql
@@ -0,0 +1,6 @@
+use belajar_database;
+SELECT * FROM produk
+ORDER BY nama ASC, harga_produk DESC;
+
+-- ASC adlah ascending yang merupakan alpabet dari A-Z dan,
+-- DESC adalah descending yang merupakan alpabet sebaliknya yaitu Z-A.
---
+++ where.sql
@@ -0,0 +1,9 @@
+use belajar_database;
+SELECT * FROM produk
+WHERE nama = 'Topi';
+
+-- where yaitu mencari sesuatu dengan otomatis, misal didalam table produk ,ada nama colom yaiut 'nama' nah kita tinngaal cari 
+
+-- contoh
+-- SELECT * FROM namatable
+-- WHERE namacolom = 'namavalues';
